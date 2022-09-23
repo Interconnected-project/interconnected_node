f79611cb3ad44d217f2bd5636678e61297bcc3c1
Tale152 <alessandro.talmi@studio.unibo.it>
2022-09-23 12:06:13 +0200

    added layer dependencies

--- src/interconnected_node/connectivity_layer/ConnectivityLayer.ts
+++ src/interconnected_node/connectivity_layer/ConnectivityLayer.ts
@@ -1,3 +1,4 @@
+import FabricLayer from '../fabric_layer/FabricLayer';
 import AuthenticationManager from './authentication_manager/AuthenticationManager';
 import ConnectionHub from './connection_hub/ConnectionHub';
 
@@ -5,6 +6,8 @@ export default class ConnectivityLayer {
   private _connectionHub: ConnectionHub = new ConnectionHub();
   private _authenticationManager: AuthenticationManager =
     new AuthenticationManager();
+
+  constructor(private fabricLayer: FabricLayer) {}
 
   public get connectionHub(): ConnectionHub {
     return this._connectionHub;
--- src/interconnected_node/resource_layer/ResourceLayer.ts
+++ src/interconnected_node/resource_layer/ResourceLayer.ts
@@ -1,3 +1,5 @@
+import FabricLayer from '../fabric_layer/FabricLayer';
+import ConnectivityLayer from '../connectivity_layer/ConnectivityLayer';
 import ClientNodeOperations from './client_node_operations/ClientNodeOperations';
 import P2POperations from './p2p_operations/P2POperations';
 
@@ -5,6 +7,11 @@ export default class ResourceLayer {
   private _clientNodeOperations: ClientNodeOperations =
     new ClientNodeOperations();
   private _p2pOperations: P2POperations = new P2POperations();
+
+  constructor(
+    private fabricLayer: FabricLayer,
+    private connectivityLayer: ConnectivityLayer
+  ) {}
 
   public get clientNodeOperations(): ClientNodeOperations {
     return this._clientNodeOperations;
