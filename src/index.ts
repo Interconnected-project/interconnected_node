a9b54d1b523cecb948e825f2fb45915ad8f5d2be
Tale152 <alessandro.talmi@studio.unibo.it>
2022-09-23 12:21:50 +0200

    exported modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-const sayHello = function (name: string) {
-  console.log('Hello ' + name + '!');
-};
-
-export default sayHello;
+export { DeviceInfo } from './interconnected_node/fabric_layer/device_info/DeviceInfo';
+export { Resources } from './interconnected_node/fabric_layer/resources/Resources';
+export { Scheduler } from './interconnected_node/fabric_layer/scheduler/Scheduler';
+export { InterconnectedNodeFactory } from './interconnected_node/InterconnectedNodeFactory';
