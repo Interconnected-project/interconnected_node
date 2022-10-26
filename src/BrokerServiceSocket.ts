2cc2b46699467abb7dbe95b7951eaf2dbd55c231
Tale152 <alessandro.talmi@studio.unibo.it>
2022-10-26 12:31:48 +0200

    working basic connection

--- src/BrokerServiceSocket.ts
+++ src/BrokerServiceSocket.ts
@@ -1,15 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { io, Socket } from 'socket.io-client';
+import Peer from 'simple-peer';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const wrtc = require('wrtc');
 
 const BROKER_SERVICE_ADDRESS =
   'http://ec2-3-208-18-248.compute-1.amazonaws.com:8000';
 
 export default class BrokerServiceSocket {
   private socket: Socket;
+  private peer: Peer.Instance | undefined;
 
   constructor(
-    id: string,
-    guiPrintCallback: (msg: string) => void,
-    backgroundPrintCallback: (msg: string) => void
+    private id: string,
+    private guiPrintCallback: (msg: string) => void,
+    private backgroundPrintCallback: (msg: string) => void
   ) {
     this.socket = io(BROKER_SERVICE_ADDRESS, {
       autoConnect: false,
@@ -18,7 +23,8 @@ export default class BrokerServiceSocket {
         role: 'NODE',
       },
     });
-    applyHandlers(this.socket, guiPrintCallback, backgroundPrintCallback);
+    this.applyHandlers();
+    this.peer = undefined;
   }
 
   connect(): void {
@@ -28,22 +34,65 @@ export default class BrokerServiceSocket {
   disconnect(): void {
     this.socket.disconnect();
   }
-}
 
-function applyHandlers(
-  socket: Socket,
-  guiPrintCallback: (msg: string) => void,
-  backgroundPrintCallback: (msg: string) => void
-): void {
-  socket.on('connect', function () {
-    guiPrintCallback('Node connected to Broker!');
-  });
+  private applyHandlers(): void {
+    this.socket.on('connect', () => {
+      this.guiPrintCallback('Node connected to Broker!');
+    });
 
-  socket.on('connect_error', (err) => {
-    guiPrintCallback('Broker connection error: ' + err.message);
-  });
+    this.socket.on('connect_error', (err) => {
+      this.guiPrintCallback('Broker connection error: ' + err.message);
+    });
 
-  socket.on('disconnect', () => {
-    guiPrintCallback('Node disconnected from Broker');
-  });
+    this.socket.on('RECRUITMENT_BROADCAST', (payload: any) => {
+      this.guiPrintCallback(
+        'Received RECRUITMENT_BROADCAST from ' +
+          payload.invokingEndpointId +
+          ' for ' +
+          payload.initiatorRole +
+          ' initiator ' +
+          payload.initiatorId
+      );
+      //TODO check for requirements and blacklist
+      payload.answererId = this.id;
+      this.socket.emit('RECRUITMENT_ACCEPT', payload);
+      this.guiPrintCallback('Node disconnected from Broker');
+      this.guiPrintCallback(
+        'Sent RECRUITMENT_ACCEPT to ' +
+          payload.initiatorRole +
+          ' initiator ' +
+          payload.initiatorId
+      );
+    });
+
+    this.socket.on('INCOMING_CONNECTION', (payload: any) => {
+      this.guiPrintCallback(
+        'Received INCOMING_CONNECTION from ' +
+          payload.initiatorRole +
+          ' initiator ' +
+          payload.initiatorId
+      );
+      this.peer = new Peer({ initiator: false, trickle: false, wrtc: wrtc });
+
+      this.peer.on('signal', (data) => {
+        this.guiPrintCallback(
+          'Sending ANSWER_CONNECTION to ' + payload.initiatorId
+        );
+        payload.signal = data;
+        this.socket.emit('ANSWER_CONNECTION', payload);
+      });
+
+      this.peer.on('connect', () => {
+        this.backgroundPrintCallback(
+          'Created P2P connection with ' + payload.initiatorId
+        );
+      });
+
+      this.peer.on('data', (data) => {
+        this.guiPrintCallback(data.toString());
+      });
+
+      this.peer.signal(payload.signal);
+    });
+  }
 }
