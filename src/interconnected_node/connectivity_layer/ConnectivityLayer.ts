4262ca1983c5400885f6cff221f60d6698be9680
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2022-10-30 18:47:48 +0100

    removed AuthenticationManager for prototype

--- src/interconnected_node/connectivity_layer/ConnectivityLayer.ts
+++ src/interconnected_node/connectivity_layer/ConnectivityLayer.ts
@@ -1,19 +1,12 @@
 import FabricLayer from '../fabric_layer/FabricLayer';
-import AuthenticationManager from './authentication_manager/AuthenticationManager';
 import ConnectionHub from './connection_hub/ConnectionHub';
 
 export default class ConnectivityLayer {
   private _connectionHub: ConnectionHub = new ConnectionHub();
-  private _authenticationManager: AuthenticationManager =
-    new AuthenticationManager();
 
   constructor(private fabricLayer: FabricLayer) {}
 
   public get connectionHub(): ConnectionHub {
     return this._connectionHub;
-  }
-
-  public get authenticationManager(): AuthenticationManager {
-    return this._authenticationManager;
   }
 }
