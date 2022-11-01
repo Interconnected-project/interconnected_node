f3cc7cec96307c4b1108292fdfdae68f05660523
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2022-11-01 15:18:10 +0100

    created Master and Slave P2PConnection interfaces

---
+++ src/P2P_connection_wrapper/MasterP2PConnection.ts
@@ -0,0 +1,4 @@
+import P2PConnectionWrapper from './P2PConnectionWrapper';
+
+//A P2P connection where my role is Master
+export default interface MasterP2PConnection extends P2PConnectionWrapper {}
---
+++ src/P2P_connection_wrapper/SlaveP2PConnection.ts
@@ -0,0 +1,4 @@
+import P2PConnectionWrapper from './P2PConnectionWrapper';
+
+//A P2P connection where my role is Slave
+export default interface SlaveP2PConnection extends P2PConnectionWrapper {}
