2176a0f49805f92bc567556ad1da99d2a73335ae
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2022-11-01 14:59:00 +0100

    blank state

--- src/common/ErrorHandling.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function throwIfNotNull(v: any, message: string): void {
-  if (v !== null) {
-    throw new Error(message);
-  }
-}
