4ea12e715e8fd4354eaaa180102c5e71d8c98d7a
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2022-11-01 19:16:53 +0100

    exported builders in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import InterconnectedNode from './interconnected_node/InterconnectedNode';
+import P2PConnectionBuilder from './interconnected_node/p2p/builders/P2PConnectionBuilder';
+import MasterP2PConnectionBuilder from './interconnected_node/p2p/builders/MasterP2PConnectionBuilder';
+import SlaveP2PConnectionBuilder from './interconnected_node/p2p/builders/SlaveP2PConnectionBuilder';
+import ClientSpecificP2PConnectionBuilders from './interconnected_node/p2p/builders/ClientSpecificP2PConnectionBuilders';
 
-export { InterconnectedNode };
+export {
+  InterconnectedNode,
+  P2PConnectionBuilder,
+  MasterP2PConnectionBuilder,
+  SlaveP2PConnectionBuilder,
+  ClientSpecificP2PConnectionBuilders,
+};
