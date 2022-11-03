79018e3b81873ebedf7c2ba1b1dc63ccfba9c7e2
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2022-11-03 10:14:15 +0100

    created JobRepository

---
+++ src/interconnected_node/contribution/common/JobsRepository.ts
@@ -0,0 +1,32 @@
+import Job from './Job';
+
+export default class JobsRepository {
+  private jobs: Array<Job> = new Array();
+
+  add(job: Job): boolean {
+    const isPresent =
+      this.jobs.find((j) => j.operationId === job.operationId) !== undefined;
+    if (isPresent) {
+      return false;
+    } else {
+      this.jobs.push(job);
+      return true;
+    }
+  }
+
+  remove(operationId: string): Job | undefined {
+    try {
+      const res = this.get(operationId);
+      if (res !== undefined) {
+        this.jobs.splice(this.jobs.indexOf(res), 1);
+      }
+      return res;
+    } catch {
+      return undefined;
+    }
+  }
+
+  get(operationId: string): Job | undefined {
+    return this.jobs.find((j) => j.operationId === operationId);
+  }
+}
